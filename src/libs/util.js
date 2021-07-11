53ddae848623fe2d62b65a218924515e260fb872
MerlotSinceFeb <expressionlessmxh@outlook.com>
2021-07-11 11:24:00 +0800

    first_commit

---
+++ src/libs/util.js
@@ -0,0 +1,8 @@
+import cookies from './utils.cookies';
+
+
+const util = {
+    cookies
+}
+
+export default util;
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { router } from './router'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css'
+import store from "./store";
+const app  = createApp(App);
+app.use(router);
+app.use(ElementPlus);
+app.use(store);
+app.mount('#app');
